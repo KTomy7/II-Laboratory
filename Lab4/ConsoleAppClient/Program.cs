ac838e8daaa82041cd43b8a24f2be0b62fd93f5a
KTomy7 <kisstomi7.kt@gmail.com>
2023-04-28 10:49:03 +0300

    Created ConsoleApp Client

---
+++ Lab4/ConsoleAppClient/Program.cs
@@ -0,0 +1,30 @@
+﻿using ConsoleAppClient.ServiceReference1;
+using Lab4Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppClient
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            WebService1SoapClient client = new WebService1SoapClient();
+            Console.WriteLine("CelsiusToFahrenheit(10) = " + client.CelsiusToFahrenheit(10));
+            Console.WriteLine("FahrenheitToCelsius(50) = " + client.FahrenheitToCelsius(50));
+            Console.WriteLine("CurrentDateAndTime() = " + client.CurrentDateAndTime());
+            Console.WriteLine("EuroToRon(10) = " + client.EuroToRon(10) + " Ron");
+            Console.WriteLine("RonToEuro(10) = " + client.RonToEuro(10) + " Euro");
+            Console.WriteLine("GetShoppingList() = ");
+            string[] shoppingList = client.GetShoppingList().ToArray();
+            for (int i = 0; i < shoppingList.Length; i++)
+            {
+                Console.WriteLine(i+1 + ". " + shoppingList[i]);
+            }
+            Console.ReadLine();
+        }
+    }
+}
