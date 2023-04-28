bb49ac372268454bda850c7801e7bd340a518d7d
KTomy7 <kisstomi7.kt@gmail.com>
2023-04-28 11:18:52 +0300

    Created WindowsFormApp Client

--- Lab4/ConsoleAppClient/Program.cs
+++ Lab4/ConsoleAppClient/Program.cs
@@ -1,10 +1,5 @@
 ﻿using ConsoleAppClient.ServiceReference1;
-using Lab4Services;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ConsoleAppClient
 {
